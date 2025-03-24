9e72abeefa4df0e4b32bc95d4545b5a6e911ed21
Shrijan Poudel <85357750+SlytherSavior@users.noreply.github.com>
2025-03-24 11:09:01 +0545

    some more functions

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+
+
+
 const getStackSource = (techstackInput) => `https://skillicons.dev/icons?i=${techstackInput}`;
 
 const getAnimatedTyping = (data) => {
-    console.log(encodeURIComponent(data).replace(/%20/g, '+'));
     const encodedData = encodeURIComponent(data) // using regex to search
         .replace(/%20/g, "+")
         .replace(/(?:\+%3B\+|\+%3B|%3B\+|%3B) ?/g, ';') //match various occurences of these pattern
-    return encodedData;
+    return `https://readme-typing-svg.demolab.com?font=Fira+Code&weight=500&size=22&duration=1500&pause=200&color=F7B733&center=true&vCenter=true&multiline=true&width=750&height=60&lines=${encodedData}`;
+}
+
+const getAchievements = (username) => {
+    return `https://github-profile-trophy.vercel.app/?username=${username}&theme=onedark`
 }
 
