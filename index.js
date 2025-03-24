a5c556f00cfc9ab79c215c4f14a9c6495d145bc4
Shrijan Poudel <85357750+SlytherSavior@users.noreply.github.com>
2025-03-24 10:59:56 +0545

    added logic for animated typing url

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const getStackSource = (techstackInput) => `https://skillicons.dev/icons?i=${techstackInput}`;
 
+const getAnimatedTyping = (data) => {
+    console.log(encodeURIComponent(data).replace(/%20/g, '+'));
+    const encodedData = encodeURIComponent(data) // using regex to search
+        .replace(/%20/g, "+")
+        .replace(/(?:\+%3B\+|\+%3B|%3B\+|%3B) ?/g, ';') //match various occurences of these pattern
+    return encodedData;
+}
+
